5d2bb0bea5e9e59c6f21b5960629836a456e7e80
Jason Snyder <jason.snyder159@gmail.com>
2022-03-22 08:48:28 -0400

    added functions to index.js

--- db/index.js
+++ db/index.js
@@ -10,10 +10,17 @@ class DB {
   findAllEmployees() {
     return this.connection.query(
       // CREATE SELECT STATMENT WITH THE FOLLOWING COLUMNS FROM THREE TABLES.
-      // id, first_name, last_name FROM employee TABLE AND department name from department TABLE AND SELECT salary FROM role TABLE
+      // id, first_name, last_name FROM employee TABLE AND
+      // department name from department TABLE
+      // AND SELECT salary FROM role TABLE
       // YOUR NEED TO USE LEFT JOINS TO JOIN THREE TABLES
-      // TODO: YOUR CODE HERE
-
+      // T*ODO: YOUR CODE HERE
+      "SELECT employee.id, employee.first_name, employee.last_name, FROM employee, \
+      department.name, FROM department, \
+      role.salary FROM role, \
+      LEFT JOIN role ON role.department_id = department.id \
+      LEFT JOIN employee ON employee.role_id = role.id \
+      GROUP BY department.id, department.name"
     );
   }
 
@@ -34,8 +41,9 @@ class DB {
   // Update the given employee's role
   updateEmployeeRole(employeeId, roleId) {
     return this.connection.query(
-      // TODO: YOUR CODE HERE
-
+      // T*ODO: YOUR CODE HERE
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
     );
   }
 
@@ -53,16 +61,19 @@ class DB {
       // SELECT THE FOLLOWING COLUMNS:
       // id, title, salary FROM role TABLE AND department name FROM department TABLE
       // YOU NEED TO USE LEFT JOIN TO JOIN role and department TABLES
-      // TODO: YOUR CODE HERE
-
+      // T*ODO: YOUR CODE HERE
+      "SELECT role.id, role.title, role.salary FROM role \
+      department.name, FROM department \
+      LEFT JOIN role on role.department_id = department.id \
+      GROUP BY department.id, department.name "
     );
   }
 
   // Create a new role
   createRole(role) {
     return this.connection.query(
-      // TODO: YOUR CODE HERE
-
+      // T*ODO: YOUR CODE HERE
+      "INSERT INTO role SET ?", role
       );
   }
 
@@ -80,7 +91,8 @@ class DB {
   // Create a new department
   createDepartment(department) {
     return this.connection.query(
-      // TODO: YOUR CODE HERE
+      // T*ODO: YOUR CODE HERE
+      "INSERT INTO department SET ?", department
     );
   }
 
@@ -99,8 +111,13 @@ class DB {
   // Find all employees by manager, join with departments and roles to display titles and department names
   findAllEmployeesByManager(managerId) {
     return this.connection.query(
-      // TODO: YOUR CODE HERE
-
+      // T*ODO: YOUR CODE HERE
+      "SELECT employee.id, employee.first)name, employee.last_name, role.title \
+      FROM employee \
+      LEFT JOIN role on employee.role_id = role.id \
+      LEFT JOIN department department on role.department_id = department.id \
+      WHERE manager.id \ ?;",
+      managerId
     );
   }
 }
